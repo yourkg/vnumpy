5cdf910933337990236520e6f4a0017040abcc4f
yankun2024 <ykyank@gmail.com>
2024-10-16 17:45:07 +0800

    up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vnumpy', 
-    version='0.1.2', 
+    version='0.1.4', 
     packages=find_packages(), 
     include_package_data=True, 
     package_data={
